c1a237a0f7f3aea4a524b306daed71cbba17df0f
Nate Lovell <Natelovell99@gmail.com>
2020-10-01 02:38:54 -0600

    Create CMYK and RGB.jsx

---
+++ CMYK and RGB.jsx
@@ -0,0 +1,18 @@
+﻿var document = app.activeDocument;
+
+document.documentColorSpace = DocumentColorSpace.RGB;
+
+//~ var cmyk = new CMYKColor();
+//~ cmyk.cyan = 80;
+//~ cmyk.magenta = 50;
+//~ cmyk.yellow = 0;
+//~ cmyk.black = 0;
+
+//~ document.defaultFillColor = cmyk;
+
+var rgb = new RGBColor();
+rgb.red = 255;
+rgb.green = 255;
+rgb.blue = 0;
+
+document.defaultStrokeColor = rgb;
